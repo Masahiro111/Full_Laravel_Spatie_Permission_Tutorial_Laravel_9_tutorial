b5cb8f264d2753825892f903cf3636d708e69bae
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-05-19 00:11:02 +0900

    Create Role Index

---
+++ resources/views/admin/permissions/index.blade.php
@@ -0,0 +1,11 @@
+<x-admin-layout>
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    You're Permission page
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,8 +24,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/admin', function () {
-    return view('admin.index');
-})->middleware(['auth', 'role:admin'])->name('admin.index');
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware(['auth', 'role:admin'])
+    ->group(function () {
+
+        Route::get('/', [IndexController::class, 'index'])
+            ->name('index');
+
+        Route::resource('/roles', RoleController::class);
+        Route::resource('/permissions', PermissionController::class);
+    });
+
+
+// Route::get('/admin', function () {
+//     return view('admin.index');
+// })->middleware(['auth', 'role:admin'])->name('admin.index');
 
 require __DIR__ . '/auth.php';
